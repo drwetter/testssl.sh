139a46d83ea66957f0326f72704c0f9f62abdfa3
Rechi <luki.rechberger@aon.at>
2015-02-24 21:52:53 +0100

    FIX: trim whitespaces (OSX)

--- testssl.sh
+++ testssl.sh
@@ -1184,7 +1184,7 @@ server_defaults() {
 		savedir=`pwd`; cd $TEMPDIR
 		$OPENSSL s_client -showcerts $STARTTLS -connect $NODEIP:$PORT $SNI 2>/dev/null </dev/null | \
      		awk -v c=-1 '/-----BEGIN CERTIFICATE-----/{inc=1;c++} inc {print > ("level" c ".crt")} /---END CERTIFICATE-----/{inc=0}'
-		nrsaved=`ls $TEMPDIR/level?.crt 2>/dev/null | wc -w`
+		nrsaved=`ls $TEMPDIR/level?.crt 2>/dev/null | wc -w | sed 's/^ *//'`
 		outln " # of certificates provided   $nrsaved"
 		cd $savedir
 
