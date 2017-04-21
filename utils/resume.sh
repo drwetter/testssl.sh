7de5e0113ba7a291d86923b579cf589bf36ea252
Dirk <dirk@testssl.sh>
2017-04-21 11:29:20 +0200

    check in

---
+++ utils/resume.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+# simple check for seesion resumption 1) by SID, 2) by tickets
+# Author: Dirk Wetter, GPLv2 see https://testssl.sh/LICENSE.txt
+
+
+echo
+echo "####################### session ID ######################"
+openssl s_client -connect $1:443 -servername $1 -bugs -no_ssl2 -no_ticket -sess_out /tmp/ssl_s </dev/null &>/dev/null
+
+echo "--------------------------------------------------------"
+openssl s_client -connect $1:443 -servername $1 -bugs -no_ssl2 -no_ticket -sess_in /tmp/ssl_s </dev/null 2>/dev/null | egrep "New|Reused|SSL handshake has read"
+echo "--------------------------------------------------------"
+
+echo "####################### session ticket ######################"
+openssl s_client -connect $1:443 -servername $1 -bugs -no_ssl2 -sess_out /tmp/ssl_s </dev/null &>/dev/null
+echo "--------------------------------------------------------"
+openssl s_client -connect $1:443 -servername $1 -bugs -no_ssl2 -sess_in /tmp/ssl_s  </dev/null 2>/dev/null| egrep "New|Reused|SSL handshake has read"
+
+echo
